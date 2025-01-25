de614a7bd97b07497ec4ed55e1d02c76739ff3db
GeorgeDoLee <Gio.dolidze1234@gmail.com>
2025-01-25 23:28:19 +0400

    added CreateProductDto

---
+++ Auction.Application/Dtos/CreateProductDto.cs
@@ -0,0 +1,19 @@
+﻿using Auction.Domain.Entities;
+
+namespace Auction.Application.Dtos;
+public class CreateProductDto
+{
+    public int UserId { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+
+    public static Product? ToProduct(CreateProductDto createProductDto)
+    {
+        return new Product
+        {
+            UserId = createProductDto.UserId,
+            Name = createProductDto.Name,
+            Description = createProductDto.Description
+        };
+    }
+}
