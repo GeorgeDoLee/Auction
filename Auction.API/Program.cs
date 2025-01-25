9173a5152c1c522a59e8c187aa0b59e56f178c1f
GeorgeDoLee <Gio.dolidze1234@gmail.com>
2025-01-25 22:00:26 +0400

    added seeder

--- Auction.API/Program.cs
+++ Auction.API/Program.cs
@@ -1,5 +1,6 @@
 using Auction.Application.Extensions;
 using Auction.Infrastructure.Extensions;
+using Auction.Infrastructure.Seeders;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,11 @@ builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
+
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<IProductSeeder>();
+
+await seeder.Seed();
 
 if (app.Environment.IsDevelopment())
 {
