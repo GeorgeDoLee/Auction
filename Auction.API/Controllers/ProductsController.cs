99e7f4d02fddb362defcf157e39f670c116837b2
GeorgeDoLee <Gio.dolidze1234@gmail.com>
2025-01-25 22:01:40 +0400

    Inject dependencies via constructor

--- Auction.API/Controllers/ProductsController.cs
+++ Auction.API/Controllers/ProductsController.cs
@@ -5,12 +5,19 @@ namespace Auction.API.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class ProductsController(IProductService productService) : ControllerBase
+public class ProductsController : ControllerBase
 {
+    private readonly IProductService _productService;
+
+    public ProductsController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
     [HttpGet]
     public async Task<IActionResult> GetAll()
     {
-        var products = await productService.GetAllProducts();
+        var products = await _productService.GetAllProducts();
         return Ok(products);
     }
 }
