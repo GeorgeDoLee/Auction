e9d65f8a2261f9ae148b18220386588cd150a589
GeorgeDoLee <Gio.dolidze1234@gmail.com>
2025-02-03 21:54:55 +0400

    remove unnecesary usings

--- Auction.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Auction.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,9 +1,6 @@
-﻿using Auction.Domain.Entities;
-using Auction.Domain.Interfaces;
+﻿using Auction.Domain.Interfaces;
 using Auction.Infrastructure.Persistance;
-using Auction.Infrastructure.Repositories;
 using Auction.Infrastructure.Seeders;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
